12981a2facf6c63e9af851e825a5465653513630
Dan Novac <danynovac@gmail.com>
2021-09-06 18:00:48 +0300

    Added entities and their relationships

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,27 +1,44 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Wishlist } from './Wishlist';
 
-@Entity({name: 'users'})
+@Entity({ name: 'users' })
 export class User {
-    @PrimaryGeneratedColumn()
+
+    @PrimaryGeneratedColumn('uuid')
     id: number;
-    @Column({length: 255, name: 'username'}  )
+
+    @Column({ length: 255, name: 'username' })
     username: string;
-    @Column({length: 255, name: 'first_name'})
+
+    @Column({ length: 255, name: 'first_name', nullable: true })
     firstName: string;
-    @Column({length: 255, name: 'last_name'})
+
+    @Column({ length: 255, name: 'last_name', nullable: true })
     lastName: string;
-    @Column({name: 'age'})
+
+    @Column({ name: 'age' })
     age: number;
-    @Column({length: 255, name: 'email'})
+
+    @Column({ length: 255, name: 'email' })
     email: string;
 
+    @OneToMany(() => Wishlist, wishlist => wishlist.user)
+    wishlists: Wishlist[];
 
-    constructor(id: number, username: string, firstName: string, lastName: string, age: number, email: string) {
-        this.id = id;
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+
+
+    constructor(username: string, firstName: string, lastName: string, age: number, email: string) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.email = email;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
     }
 }
